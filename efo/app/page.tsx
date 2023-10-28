7c7a3e6912da6e84ac11209a617093fcb1615ab1
Maxim Werthmann <117868254+maximwerthmann@users.noreply.github.com>
2023-10-28 20:35:27 +0200

    Update page.tsx

--- efo/app/page.tsx
+++ efo/app/page.tsx
@@ -15,18 +15,18 @@ export default function Home() {
         </div>
       </header>
       <div className="flex-1 h-full w-full bg-[url('/background.jpg')] bg-cover flex items-center justify-center">
-        <div className='flex flex-col p-8 bg-gray-900/70 backdrop-blur-md space-y-8 max-w-[700px] rounded-2xl items-center justify-center'>
+        <div className='flex flex-col p-8 bg-neutral-900/70 backdrop-blur-md space-y-8 max-w-[700px] rounded-2xl items-center justify-center'>
           <Image height={200} width={200} alt='efo-logo' className='' src={'/EFO_Logo.svg'}/>
           <span className='text-center font-bold text-4xl'>
             Seite im Aufbau und VoG in Gründung.
           </span>
           <span className='text-center font-semibold text-xl'>
-            Hier bald die neue seite des neuen ostbelgischen Eisenbahnvereines.
+            Hier bald die neue Seite des neuen ostbelgischen Eisenbahnvereines.
           </span>
         </div>
-        <div className='flex flex-col p-8 bg-gray-900/70 backdrop-blur-md space-y-16 max-w-[750px] rounded-2xl absolute right-8 bottom-8'>
+        <div className='flex flex-col p-8 bg-neutral-900/70 backdrop-blur-md space-y-16 max-w-[750px] rounded-2xl absolute right-8 bottom-8'>
         <div className='flex items-center justify-center space-x-8'>
-          <span className='text-red-500'>Hier geht&apos;s zur unserem Schwesterverein:<br/> Eisenbahnfreunde Grenzland eV</span>
+          <span className='text-red-500'>Hier geht&apos;s zu unserem Schwesterverein:<br/> Eisenbahnfreunde Grenzland eV</span>
             <Link href={'https://eisenbahnfreunde-grenzland.de/'} className='py-2 px-8 text-center bg-white rounded-md text-black font-bold hover:bg-gray-300 transition-colors duration-200 flex space-x-2 items-center'>
             <span>Zur</span>
               <Image height={36} width={40} alt='efg-logo' src={'/EFG_Logo.png'} />
